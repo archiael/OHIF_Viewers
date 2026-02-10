3e3fe21578cce5835cebf90582016b0eb3155879
htkim <hyeontae.kim@medicalpark.co.kr>
2026-02-10 13:40:32 +0900

    Measurements 목록 스크롤 강제 활성화
    (cherry picked from commit a1c607352601af7a12d0cb4ba9be8b70e78ba961)

--- platform/ui-next/src/components/MeasurementTable/MeasurementTable.tsx
+++ platform/ui-next/src/components/MeasurementTable/MeasurementTable.tsx
@@ -1,6 +1,13 @@
 import React from 'react';
 import { useTranslation } from 'react-i18next';
-import { Icons, PanelSection, ScrollArea, Tooltip, TooltipContent, TooltipTrigger } from '../../index';
+import {
+  Icons,
+  PanelSection,
+  ScrollArea,
+  Tooltip,
+  TooltipContent,
+  TooltipTrigger,
+} from '../../index';
 import DataRow from '../DataRow/DataRow';
 import { createContext } from '../../lib/createContext';
 
@@ -45,7 +52,7 @@ const MeasurementTable = ({
           <span>{`${t(title)} (${amount})`}</span>
         </PanelSection.Header>
         <PanelSection.Content key="measurementTableContent">
-          <ScrollArea className="max-h-[calc(100vh-200px)]">{children}</ScrollArea>
+          <ScrollArea className="max-h-[calc(100vh-200px)] overflow-y-auto">{children}</ScrollArea>
         </PanelSection.Content>
       </PanelSection>
     </MeasurementTableProvider>
@@ -129,9 +136,7 @@ const Row = ({ item, index }: RowProps) => {
       isVisible={item.isVisible}
       isLocked={item.isLocked}
     >
-      {item.isUnmapped && (
-        <DataRow.Status.Warning tooltip={item.statusTooltip} />
-      )}
+      {item.isUnmapped && <DataRow.Status.Warning tooltip={item.statusTooltip} />}
     </DataRow>
   );
 };
